ae430d3d5da759d0238c79f563e0db23d363e51b
Rafael Correa <scriptsengineer@gmail.com>
2022-12-11 00:44:39 -0300

    Add has func and get amount of func

--- addons/inventory-system/core/inventory.gd
+++ addons/inventory-system/core/inventory.gd
@@ -1,28 +1,67 @@
 extends Node
 class_name Inventory
 
+signal inventory_changed
+
 @export var fixed_size := false
-@export var slots = [{ "id" = 0, "amount" = 0, "capacity" = 16}]
+@export var slots = [{ "item" = 0, "amount" = 0, "capacity" = 16}]
 
 
-func add(id : int, amount := 1) -> int:
-	var amount_to_add = amount
+func add(item : int, amount := 1) -> int:
+	var amount_in_interact = amount
 	for slot in slots:
-		amount_to_add = _add_to_slot(slot, id, amount)
-#	if amount_to_add > 0 and not fixed_size:
-#		slots.append({ "id" = 0 })
-	return amount_to_add
+		amount_in_interact = _add_to_slot(slot, item, amount_in_interact)
+	if amount_in_interact > 0 and not fixed_size:
+		var slot = { "item" = 0, "amount" = 0, "capacity" = 16}
+		slots.append(slot)
+		amount_in_interact = _add_to_slot(slot, item, amount_in_interact)
+	_check_if_inventory_changed(amount, amount_in_interact)
+	return amount_in_interact
 
-func remove(id : int, amount := 1) -> int:
-	var amount_to_remove = amount
+
+func remove(item : int, amount := 1) -> int:
+	var amount_in_interact = amount
+	var index := 0
 	for slot in slots:
-		amount_to_remove = _remove_from_slot(slot, id, amount)
-	return amount_to_remove
+		amount_in_interact = _remove_from_slot(slot, item, amount_in_interact)
+		if slot.amount == 0 and not fixed_size:
+			slots.remove_at(index)
+		index += 1
+	_check_if_inventory_changed(amount, amount_in_interact)
+	return amount_in_interact
+
+
+func has(item : int, amount := 1) -> bool:
+	var amount_in_inventory := 0
+	for slot in slots:
+		if slot.item == item:
+			amount_in_inventory += slot.amount
+			if amount_in_inventory > amount:
+				return true
+	return false
+
+
+func get_amount_of(item : int) -> int:
+	var amount := 0
+	for slot in slots:
+		if slot.item == item:
+			amount += slot.amount
+	return amount
+
+
+func clear():
+	slots.clear()
+
+
+#Signals
+func _check_if_inventory_changed(amount, amount_in_interact):
+	if amount != 0 and amount != amount_in_interact:
+		emit_signal("inventory_changed")
 
 
 #Slots
 func _add_to_slot(slot : Dictionary, item : int, amount := 1) -> int:
-	if amount <= 0:
+	if amount <= 0 or (slot.item != item and slot.item != 0):
 		return amount
 	var amount_to_add = min(amount, slot.capacity - slot.amount)
 	slot.amount += amount_to_add
@@ -33,7 +72,7 @@ func _add_to_slot(slot : Dictionary, item : int, amount := 1) -> int:
 
 
 func _remove_from_slot(slot : Dictionary, item : int, amount := 1) -> int:
-	if amount <= 0:
+	if amount <= 0 or slot.item != item:
 		return amount
 	var amount_to_remove = min(amount, slot.amount)
 	slot.amount -= amount_to_remove
--- addons/inventory-system/demos/fps/basic_test.gd
+++ addons/inventory-system/demos/fps/basic_test.gd
@@ -6,5 +6,22 @@ var inventory_path := NodePath("Inventory")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	inventory.add(1, 1)
+	if Input.is_action_just_released("Add item"):
+		inventory.add(1, 1)
+	if Input.is_action_just_released("Remove item"):
+		inventory.remove(1, 1)
+		
+	if Input.is_action_just_released("Add item B"):
+		inventory.add(2, 1)
+	if Input.is_action_just_released("Remove item B"):
+		inventory.remove(2, 1)
+		
+	if Input.is_action_just_released("Has"):
+		print(inventory.has(2))
+	if Input.is_action_just_released("GetAmountOf"):
+		print(inventory.get_amount_of(2))
+
+func _on_inventory_inventory_changed():
+	print("Inventory")
 	print(inventory.slots)
+	print("\n")
