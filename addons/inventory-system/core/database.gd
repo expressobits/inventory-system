dbbf927f03742d92d158bc16eb44936152d9f488
Rafael Correa <scriptsengineer@gmail.com>
2023-01-22 18:38:24 -0300

    Handler node3D to node

--- addons/inventory-system/core/database.gd
+++ addons/inventory-system/core/database.gd
@@ -12,27 +12,31 @@ class_name InventoryDatabase
 #	"hand_item", packedScene,
 #}
 
+
 func get_id_from_item(item : Item) -> int:
 	for item_data in items:
 		if item_data.item == item:
 			return item_data.id
 	printerr("item ",item," is not in the database!")
 	return -1
-	
+
+
 func get_id_from_pickable_item(pickable_item : PickableItem) -> int:
 	for item_data in items:
 		if item_data.pickable_item == pickable_item:
 			return item_data.id
 	printerr("pickable_item ",pickable_item," is not in the database!")
 	return -1
-	
+
+
 func get_item(id : int) -> Item:
 	for item_data in items:
 		if item_data.id == id:
 			return item_data.item
 	printerr("id ",id," is not in the database!")
 	return null
-	
+
+
 func get_pickable_item(id : int) -> PackedScene:
 	for item_data in items:
 		if item_data.id == id:
--- addons/inventory-system/core/inventory_handler.gd
+++ addons/inventory-system/core/inventory_handler.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends Node
 class_name InventoryHandler
 
 signal dropped(pickable_item : PickableItem)
