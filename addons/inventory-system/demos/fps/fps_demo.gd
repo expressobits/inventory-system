384a1cee34b161f5b3d1c70a1273902ca6fda100
Rafael Correa <scriptsengineer@gmail.com>
2023-02-12 12:01:41 -0300

    Add mp demo scene

--- addons/inventory-system/demos/fps/fps_demo.gd
+++ addons/inventory-system/demos/fps/fps_demo.gd
@@ -1,7 +1,6 @@
-extends Node3D
+extends Node
+class_name FPSDemo
 
-var player_inventory_handler_path := NodePath("Player/InventoryHandler")
-@onready var player_inventory_handler: InventoryHandler = get_node(player_inventory_handler_path)
 
 var inventory_system_ui_path := NodePath("UI/Inventory System UI")
 @onready var inventory_system_ui: Control = get_node(inventory_system_ui_path)
@@ -10,13 +9,19 @@ var inventory_system_ui_path := NodePath("UI/Inventory System UI")
 @export var item_metal : InventoryItem
 @export var database : InventoryDatabase
 
+var player_inventory_handler : InventoryHandler
+
+
 func _ready():
-	inventory_system_ui.set_player_inventory_handler(player_inventory_handler)
-	player_inventory_handler.inventory.emptied.connect(_on_empty.bind())
-	player_inventory_handler.inventory.filled.connect(_on_filled.bind())
-	player_inventory_handler.opened.connect(_update_opened_inventories.bind())
-	player_inventory_handler.closed.connect(_update_opened_inventories.bind())
-	_update_opened_inventories(player_inventory_handler.inventory)
+	setup_inventory_handler(get_node(NodePath("Player/InventoryHandler")))
+
+
+func setup_inventory_handler(inventory_handler : InventoryHandler):
+	player_inventory_handler = inventory_handler
+	inventory_system_ui.set_player_inventory_handler(inventory_handler)
+	inventory_handler.opened.connect(_update_opened_inventories.bind())
+	inventory_handler.closed.connect(_update_opened_inventories.bind())
+	_update_opened_inventories(inventory_handler.inventory)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,12 +36,6 @@ func _process(delta):
 	if Input.is_action_just_released("remove_item_b"):
 		player_inventory_handler.inventory.remove(item_metal, 2)
 
-
-func _on_empty():
-	print("EMPTY")
-
-func _on_filled():
-	print("FILLED")
 
 func _update_opened_inventories(inventory : Inventory):
 	if player_inventory_handler.is_open_main_inventory():
---
+++ addons/inventory-system/demos/mp/mp_demo.gd
@@ -0,0 +1,67 @@
+extends FPSDemo
+class_name MPDemo
+
+@export var player_scene : PackedScene
+
+var connected_peer_ids : Array
+var players : Array
+var peer : ENetMultiplayerPeer
+
+func _ready():
+	multiplayer.peer_connected.connect(_player_connected.bind())
+#	multiplayer.peer_disconnected.connect(_player_disconnected.bind())
+#	multiplayer.connected_to_server.connect(_connected_ok.bind())
+#	multiplayer.connection_failed.connect(_connected_fail.bind())
+#	multiplayer.server_disconnected.connect(_server_disconnected.bind())
+
+func create_player(peer_id : int):
+	connected_peer_ids.append(peer_id)
+	var player = player_scene.instantiate()
+	player.name = str(peer_id)
+	players.append(player)
+	player.position = Vector3(0,2,0)
+	add_child(player)
+	if peer_id == multiplayer.get_unique_id():
+		print("local")
+		setup_inventory_handler(player.get_node("InventoryHandler"))
+
+
+@rpc	
+func add_newly_connected_player_character(new_peer_id):
+	create_player(new_peer_id)
+
+
+@rpc
+func add_previously_connected_player_characters(peer_ids):
+	for peer_id in peer_ids:
+		create_player(peer_id)	
+
+
+func host_game():
+	peer = ENetMultiplayerPeer.new()
+	peer.create_server(4242)
+	multiplayer.set_multiplayer_peer(peer)
+	create_player(multiplayer.get_unique_id())
+	$"UI/Connect Panel".visible = false
+
+
+func connect_to_ip(ip):
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(ip, 4242)
+	multiplayer.set_multiplayer_peer(peer)
+	$"UI/Connect Panel".visible = false
+
+
+func _player_connected(new_peer_id : int):
+	if multiplayer.is_server():
+		add_newly_connected_player_character.rpc(new_peer_id)
+		add_previously_connected_player_characters.rpc_id(new_peer_id, connected_peer_ids)
+		create_player(new_peer_id)
+
+
+func _on_host_button_button_down():
+	host_game()
+
+
+func _on_connect_button_button_down():
+	connect_to_ip("localhost")
