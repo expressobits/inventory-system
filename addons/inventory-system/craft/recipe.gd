c2932a066a0383dc933bb7e12badcb35edd0a370
Rafael Correa <scriptsengineer@gmail.com>
2023-03-20 21:19:27 -0300

    Documentation Reference API for craft_station

--- addons/inventory-system/craft/recipe.gd
+++ addons/inventory-system/craft/recipe.gd
@@ -3,7 +3,7 @@ extends Resource
 class_name Recipe
 
 @export var product : Slot
-@export var subproducts : Array[Slot]
+@export var byproducts : Array[Slot]
 @export var time_to_craft := 4.0
 @export var station : CraftStationType
 @export var ingredients : Array[Slot]
