938d4cf6b7ef16cdff905971b55451aaa5de028e
Rafael Correa <scriptsengineer@gmail.com>
2023-03-22 19:53:43 -0300

    Add main screen

--- addons/inventory-system/inventory-system.gd
+++ addons/inventory-system/inventory-system.gd
@@ -1,12 +1,40 @@
 @tool
 extends EditorPlugin
 
+const icon_plugin = preload("res://addons/inventory-system/icons/inventory_main_screen.svg")
+const inventory_editor_scene = preload("res://addons/inventory-system/editor/inventory_editor.tscn")
+
+var _inventory_editor : Control
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	pass
+	_inventory_editor = inventory_editor_scene.instantiate()
+	_inventory_editor.name = "Inventory System"
+	get_editor_interface().get_editor_main_screen().add_child(_inventory_editor)
+#	_inventory_editor.set_editor(self)
+#	_inventory_editor_plugin = preload("res://addons/inventory-system/editor/inventory-system-inspector.gd").new()
+#	add_inspector_plugin()
 
 
 func _exit_tree():
+	if _inventory_editor:
+		_inventory_editor.queue_free()
 	# Clean-up of the plugin goes here.
-	pass
+#	remove_inspector_plugin()
+
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	if _inventory_editor:
+		_inventory_editor.visible = visible
+
+
+func _get_plugin_name():
+	return "Inventory"
+
+
+func _get_plugin_icon():
+	return icon_plugin
