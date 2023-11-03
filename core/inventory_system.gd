b571d6eb3970ad46ba813bd8ec885bf93f5313f4
Rafael Correa <scriptsengineer@gmail.com>
2023-11-03 20:14:23 -0300

    Add singleton

---
+++ core/inventory_system.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var inventory_handler : InventoryHandler
+var crafter : Crafter
+
+
+signal inventory_handler_changed
+signal crafter_changed
+
+
+func setup_inventory_handler(inventory_handler : InventoryHandler):
+	self.inventory_handler = inventory_handler
+	inventory_handler_changed.emit()
+
+
+func setup_crafter(crafter : Crafter):
+	self.crafter = crafter
+	crafter_changed.emit()
