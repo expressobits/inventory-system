0a81aa0b0faae57b1925e41c1b5d3ee19fd51b9e
Rafael Correa <scriptsengineer@gmail.com>
2024-05-12 12:19:09 -0300

    Update loading library to module level servers

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -60,7 +60,7 @@ GDExtensionBool GDE_EXPORT inventory_system_init(GDExtensionInterfaceGetProcAddr
 	GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 	init_obj.register_initializer(initialize_gdextension_types);
 	init_obj.register_terminator(uninitialize_gdextension_types);
-	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SERVERS);
 
 	return init_obj.init();
 }
