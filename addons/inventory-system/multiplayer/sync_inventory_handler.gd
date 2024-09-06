508047ae765603363b1c2d7cb8c2428f9d43711e
Rafael Correa <scriptsengineer@gmail.com>
2024-09-05 22:37:30 -0300

    Fix sync inventory handler problem
    # Conflicts:
    #	addons/inventory-system/multiplayer/sync_inventory_handler.gd
    #	bin/windows/inventory_system.windows.template_debug.x86_64.dll

--- addons/inventory-system/multiplayer/sync_inventory_handler.gd
+++ addons/inventory-system/multiplayer/sync_inventory_handler.gd
@@ -5,8 +5,6 @@ class_name SyncInventoryHandler
 
 func _ready() -> void:
 	inventory_handler.updated_transaction_slot.connect(on_updated_transaction_slot)
-	inventory_handler.opened.connect(on_opened)
-	inventory_handler.closed.connect(on_closed)
 
 	
 func on_updated_transaction_slot():
@@ -19,38 +17,9 @@ func on_updated_transaction_slot():
 		item_id = inventory_handler.transaction_slot.item.definition.id
 	updated_transaction_slot_rpc.rpc(item_id, inventory_handler.transaction_slot.amount)
 
-
-func on_opened(inventory : Inventory):
-	if not multiplayer.is_server():
-		return
-	opened_rpc.rpc(inventory_handler.get_path_to(inventory))
-
-
-func on_closed(inventory : Inventory):
-	if not multiplayer.is_server():
-		return
-	closed_rpc.rpc(inventory_handler.get_path_to(inventory))
-
 @rpc
 func updated_transaction_slot_rpc(item_id : int, amount : int):
 	var definition = inventory_handler.database.get_item(item_id)
 	var item = Item.new()
 	item.definition = definition
 	inventory_handler.change_transaction_slot(item, amount);
-
-
-@rpc
-func opened_rpc(inventory_path : NodePath):
-	var inventory = inventory_handler.get_node(inventory_path)
-	inventory_handler.opened_inventories.append(inventory_handler.get_path_to(inventory))
-	inventory_handler.opened.emit(inventory)
-
-
-@rpc
-func closed_rpc(inventory_path : NodePath):
-	var inventory = inventory_handler.get_node(inventory_path)
-	var index = inventory_handler.opened_inventories.find(inventory_handler.get_path_to(inventory))
-	if index == -1:
-		return
-	inventory_handler.opened_inventories.remove_at(index)
-	inventory_handler.closed.emit(inventory)
