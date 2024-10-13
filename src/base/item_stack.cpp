6eac07c03383e15368b16604e7a3794d097f0fda
Rafael Correa <scriptsengineer@gmail.com>
2024-10-13 12:53:32 -0300

    Add properties for item stack

--- src/base/item_stack.cpp
+++ src/base/item_stack.cpp
@@ -5,9 +5,12 @@ void ItemStack::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_item_id"), &ItemStack::get_item_id);
 	ClassDB::bind_method(D_METHOD("set_amount", "amount"), &ItemStack::set_amount);
 	ClassDB::bind_method(D_METHOD("get_amount"), &ItemStack::get_amount);
+	ClassDB::bind_method(D_METHOD("set_properties", "properties"), &ItemStack::set_properties);
+	ClassDB::bind_method(D_METHOD("get_properties"), &ItemStack::get_properties);
 
 	ADD_PROPERTY(PropertyInfo(Variant::STRING, "item_id"), "set_item_id", "get_item_id");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "amount"), "set_amount", "get_amount");
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "properties"), "set_properties", "get_properties");
 }
 
 ItemStack::ItemStack() {
@@ -32,6 +35,14 @@ int ItemStack::get_amount() const {
 	return amount;
 }
 
+void ItemStack::set_properties(const Dictionary &new_properties) {
+	properties = new_properties;
+}
+
+Dictionary ItemStack::get_properties() const {
+	return properties;
+}
+
 String ItemStack::serialize() const {
 	String data = String();
 	if (!item_id.is_empty())
@@ -40,6 +51,8 @@ String ItemStack::serialize() const {
 		data += "";
 	data += " ";
 	data += String::num_int64(get_amount());
+	data += " ";
+	data += serialize_properties(properties);
 	return data;
 }
 
@@ -50,4 +63,35 @@ void ItemStack::deserialize(String data) {
 		set_item_id(array[0]);
 	if (!array[1].is_empty())
 		set_amount(array[1].to_int());
+	String properties_data = String();
+	for (size_t i = 2; i < array.size(); i++) {
+		properties_data += array[i];
+		if (i < array.size() - 1)
+			properties_data += array[i];
+	}
+	set_properties(deserialize_properties(properties_data));
+}
+
+String ItemStack::serialize_properties(const Dictionary properties) {
+	String properties_data = String();
+	for (size_t i = 0; i < properties.keys().size(); i++) {
+		properties_data += properties.keys()[i];
+		properties_data += ":";
+		properties_data += properties[properties.keys()[i]];
+		if (i < properties.keys().size() - 1)
+			properties_data += " ";
+	}
+	return properties_data;
+}
+
+Dictionary ItemStack::deserialize_properties(const String data) {
+	Dictionary properties = Dictionary();
+	PackedStringArray array = data.split(" ");
+	for (size_t i = 0; i < array.size(); i++) {
+		PackedStringArray key_value = array[i].split(":");
+		if (key_value.size() != 2)
+			continue;
+		properties[key_value[0]] = key_value[1];
+	}
+	return properties;
 }
--- src/base/item_stack.h
+++ src/base/item_stack.h
@@ -11,6 +11,7 @@ class ItemStack : public Resource {
 private:
     String item_id = "";
     int amount = 0;
+	Dictionary properties;
 
 protected:
 	static void _bind_methods();
@@ -22,8 +23,13 @@ public:
 	String get_item_id() const;
 	void set_amount(const int &new_amount);
 	int get_amount() const;
+	void set_properties(const Dictionary &new_properties);
+	Dictionary get_properties() const;
     String serialize() const;
     void deserialize(String data);
+
+	static String serialize_properties(const Dictionary properties);
+	static Dictionary deserialize_properties(const String properties_data);
 };
 
 #endif // ITEM_STACK_CLASS_H
