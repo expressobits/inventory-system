73e8fe534edac819aa4f7fc350943505e7d9c850
Rafael Correa <scriptsengineer@gmail.com>
2024-11-14 14:35:30 -0300

    Add new hotbar for grid inventory

--- src/core/hotbar.cpp
+++ src/core/hotbar.cpp
@@ -1,8 +1,17 @@
 #include "hotbar.h"
+#include <godot_cpp/variant/utility_functions.hpp>
 
-void Hotbar::_on_updated_stack(const int stack_index) {
-	if (stack_index == selection_index) {
-		emit_signal("on_update_selection_stack");
+void Hotbar::_on_contents_changed() {
+	for (size_t i = 0; i < equipped_stacks.size(); i++) {
+		Ref<ItemStack> stack = equipped_stacks[i];
+		if (stack == nullptr)
+			continue;
+		Inventory *inventory = get_inventory();
+		int stack_index = inventory->get_stacks().find(stack);
+		if (stack_index == -1) {
+			equipped_stacks[i] = nullptr;
+			emit_signal("equipped_stack_changed", i);
+		}
 	}
 }
 
@@ -10,21 +19,23 @@ void Hotbar::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_inventory_path", "inventory"), &Hotbar::set_inventory_path);
 	ClassDB::bind_method(D_METHOD("get_inventory_path"), &Hotbar::get_inventory_path);
 	ClassDB::bind_method(D_METHOD("get_inventory"), &Hotbar::get_inventory);
-	ClassDB::bind_method(D_METHOD("set_slots_in_hot_bar", "slots_in_hot_bar"), &Hotbar::set_slots_in_hot_bar);
-	ClassDB::bind_method(D_METHOD("get_slots_in_hot_bar"), &Hotbar::get_slots_in_hot_bar);
+	ClassDB::bind_method(D_METHOD("set_slots_count", "slots_count"), &Hotbar::set_slots_count);
+	ClassDB::bind_method(D_METHOD("get_slots_count"), &Hotbar::get_slots_count);
 	ClassDB::bind_method(D_METHOD("set_selection_index", "selection_index"), &Hotbar::set_selection_index);
 	ClassDB::bind_method(D_METHOD("get_selection_index"), &Hotbar::get_selection_index);
+	ClassDB::bind_method(D_METHOD("equip", "stack", "slot_index"), &Hotbar::equip);
 	ClassDB::bind_method(D_METHOD("next_item"), &Hotbar::next_item);
 	ClassDB::bind_method(D_METHOD("previous_item"), &Hotbar::previous_item);
 	ClassDB::bind_method(D_METHOD("has_valid_item_id"), &Hotbar::has_valid_item_id);
 	ClassDB::bind_method(D_METHOD("has_item_on_selection"), &Hotbar::has_item_on_selection);
-	ClassDB::bind_method(D_METHOD("get_selected_item"), &Hotbar::get_selected_item);
+	ClassDB::bind_method(D_METHOD("get_selected_item_id"), &Hotbar::get_selected_item_id);
+	ClassDB::bind_method(D_METHOD("get_selected_stack"), &Hotbar::get_selected_stack);
 
 	ADD_SIGNAL(MethodInfo("on_change_selection", PropertyInfo(Variant::INT, "selection_index")));
-	ADD_SIGNAL(MethodInfo("on_update_selection_stack"));
+	ADD_SIGNAL(MethodInfo("equipped_stack_changed", PropertyInfo(Variant::INT, "slot_index")));
 
 	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "inventory", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Inventory"), "set_inventory_path", "get_inventory_path");
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "slots_in_hot_bar"), "set_slots_in_hot_bar", "get_slots_in_hot_bar");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "slots_count"), "set_slots_count", "get_slots_count");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "selection_index"), "set_selection_index", "get_selection_index");
 }
 
@@ -50,22 +61,31 @@ Inventory *Hotbar::get_inventory() const {
 	return inventory;
 }
 
-void Hotbar::set_slots_in_hot_bar(const int &new_slots_in_hot_bar) {
-	slots_in_hot_bar = new_slots_in_hot_bar;
+void Hotbar::set_slots_count(const int &new_slots_count) {
+	slots_count = new_slots_count;
+	if (slots_count <= 0)
+		slots_count = 1;
+
+	for (size_t i = equipped_stacks.size(); i < slots_count; i++) {
+		equipped_stacks.append(nullptr);
+	}
+	for (size_t i = equipped_stacks.size() - 1; i >= slots_count; i--) {
+		equipped_stacks.remove_at(i);
+	}
 }
 
-int Hotbar::get_slots_in_hot_bar() const {
-	return slots_in_hot_bar;
+int Hotbar::get_slots_count() const {
+	return slots_count;
 }
 
 void Hotbar::set_selection_index(const int &new_selection_index) {
 	int old_selection = selection_index;
 	selection_index = new_selection_index;
-	if (selection_index >= slots_in_hot_bar) {
-		selection_index -= slots_in_hot_bar;
+	if (selection_index >= slots_count) {
+		selection_index -= slots_count;
 	}
 	if (selection_index < 0) {
-		selection_index += slots_in_hot_bar;
+		selection_index += slots_count;
 	}
 	if (old_selection != selection_index && is_node_ready()) {
 		emit_signal("on_change_selection", selection_index);
@@ -74,6 +94,18 @@ void Hotbar::set_selection_index(const int &new_selection_index) {
 
 int Hotbar::get_selection_index() const {
 	return selection_index;
+}
+
+void Hotbar::equip(Ref<ItemStack> stack, int slot_index) {
+	equipped_stacks[slot_index] = stack;
+	emit_signal("equipped_stack_changed", slot_index);
+}
+
+void Hotbar::unequip(Ref<ItemStack> stack) {
+	int slot_index = equipped_stacks.find(stack);
+	if (slot_index == -1)
+		return;
+	equipped_stacks[slot_index] = nullptr;
 }
 
 void Hotbar::next_item() {
@@ -86,10 +118,14 @@ void Hotbar::previous_item() {
 
 bool Hotbar::has_valid_item_id() const {
 	Inventory *inventory = get_inventory();
-	if (selection_index >= inventory->get_stacks().size()) {
+	Ref<ItemStack> stack = equipped_stacks[selection_index];
+	int stack_index = inventory->get_stacks().find(stack);
+	if (stack_index == -1) {
 		return false;
+	} else {
+		stack = inventory->get_stacks()[stack_index];
 	}
-	Ref<ItemStack> stack = inventory->get_stacks()[selection_index];
+
 	if (stack == nullptr) {
 		return false;
 	}
@@ -100,10 +136,17 @@ bool Hotbar::has_item_on_selection() const {
 	return has_valid_item_id();
 }
 
-String Hotbar::get_selected_item() const {
-	Inventory *inventory = get_inventory();
-	Ref<ItemStack> stack = inventory->get_stacks()[selection_index];
+String Hotbar::get_selected_item_id() const {
+	Ref<ItemStack> stack = get_selected_stack();
+	if (stack == nullptr) {
+		return "";
+	}
 	return stack->get_item_id();
+}
+
+Ref<ItemStack> Hotbar::get_selected_stack() const {
+	Ref<ItemStack> stack = equipped_stacks[selection_index];
+	return stack;
 }
 
 Hotbar::Hotbar() {
@@ -124,7 +167,7 @@ void Hotbar::_ready() {
 		return;
 	}
 	if (inventory != nullptr) {
-		inventory->connect("updated_stack", callable_mp(this, &Hotbar::_on_updated_stack));
+		inventory->connect("contents_changed", callable_mp(this, &Hotbar::_on_contents_changed));
 	}
 	NodeInventories::_ready();
 }
--- src/core/hotbar.h
+++ src/core/hotbar.h
@@ -2,7 +2,6 @@
 #define HOTBAR_CLASS_H
 
 #include "base/node_inventories.h"
-#include "base/slot.h"
 #include "inventory.h"
 
 using namespace godot;
@@ -12,9 +11,10 @@ class Hotbar : public NodeInventories {
 
 private:
 	NodePath inventory_path;
-	int slots_in_hot_bar = 8;
-	int selection_index = 0;
-	void _on_updated_stack(const int slot_index);
+	int slots_count = 4;
+	int selection_index = -1;
+	TypedArray<ItemStack> equipped_stacks;
+	void _on_contents_changed();
 
 protected:
 	static void _bind_methods();
@@ -26,15 +26,19 @@ public:
 	void set_inventory_path(const NodePath &new_inventory);
 	NodePath get_inventory_path() const;
 	Inventory *get_inventory() const;
-	void set_slots_in_hot_bar(const int &new_slots_in_hot_bar);
-	int get_slots_in_hot_bar() const;
+	void set_slots_count(const int &new_slots_count);
+	int get_slots_count() const;
 	void set_selection_index(const int &new_selection_index);
 	int get_selection_index() const;
+
+	void equip(Ref<ItemStack> stack, int slot_index = 0);
+	void unequip(Ref<ItemStack> stack);
 	void next_item();
 	void previous_item();
 	bool has_valid_item_id() const;
 	bool has_item_on_selection() const;
-	String get_selected_item() const;
+	String get_selected_item_id() const;
+	Ref<ItemStack> get_selected_stack() const;
 };
 
 #endif // HOTBAR_CLASS_H
