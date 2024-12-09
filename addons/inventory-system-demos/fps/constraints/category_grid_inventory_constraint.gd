74904c50c3323e5c3177e20e4799e727f77ca517
Rafael Correa <scriptsengineer@gmail.com>
2024-12-09 20:34:32 -0300

    Fix category grid inventory constraint

--- addons/inventory-system-demos/fps/constraints/category_grid_inventory_constraint.gd
+++ addons/inventory-system-demos/fps/constraints/category_grid_inventory_constraint.gd
@@ -5,15 +5,34 @@ class_name CategoryGridInventoryConstraint
 @export var is_only_one_category_for_slots: bool
 @export var one_category_for_slots: String
 
-func _can_add_on_position(inventory: Node, position: Vector2i, item_id: String, amount: int, properties: Dictionary, is_rotated: bool):
-	var category: String = get_category(position)
+func _can_add_on_position(inventory: Node, position: Vector2i, item_id: String, _amount: int, _properties: Dictionary, _is_rotated: bool):
+	var def: ItemDefinition = inventory.database.get_item(item_id)
+	if def == null:
+		return false
+	
+	var size: Vector2i
+	if _is_rotated:
+		size = Vector2i(def.size.y, def.size.x)
+	else:
+		size = def.size
+	
+	for x in range(position.x,position.x + size.x):
+		for y in range(position.y,position.y + size.y):
+			var pos = Vector2i(x, y)
+			var category: String = get_category(pos)
+			if not contains_category(def, category):
+				return false
+	return true
+
+
+func contains_category(def: ItemDefinition, category: String):
 	if category != "":
-		var def : ItemDefinition = inventory.database.get_item(item_id)
 		for c in def.categories:
 			if c.id == category:
 				return true
 		return false
-	return true
+	else:
+		return true
 
 func get_category(position: Vector2i) -> String:
 	if is_only_one_category_for_slots:
