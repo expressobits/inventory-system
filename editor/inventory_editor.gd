76db91ec5c8763be8e8c279c2b9858ef0578b52e
FlauschBert <github@shaula.de>
2025-04-04 17:15:21 +0200

    add new item description

--- editor/inventory_editor.gd
+++ editor/inventory_editor.gd
@@ -245,6 +245,7 @@ func _on_export_menu_id_pressed(id: int) -> void:
 
 func _on_new_item_menu_id_pressed() -> void:
 	var new_item_definition = ItemDefinition.new()
+	new_item_definition.name = "New Item"
 	database.add_new_item(new_item_definition)
 	save_file()
 	load_database(database)
