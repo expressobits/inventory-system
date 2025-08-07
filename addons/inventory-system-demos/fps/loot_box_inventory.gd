15d71f2d7775be0d252696dd26042857e7ebea68
Rafael Correa <scriptsengineer@gmail.com>
2025-08-06 21:13:42 -0300

    Add loot items to fps demo

---
+++ addons/inventory-system-demos/fps/loot_box_inventory.gd
@@ -0,0 +1,13 @@
+extends BoxInventory
+class_name LootBoxInventory
+
+@onready var loot_generator: LootGenerator = $LootGenerator
+
+func _on_open():
+	loot_generator.generate_loot_count(3)
+	super._on_open()
+
+
+func _on_close():
+	inventory.clear()
+	super._on_close()
